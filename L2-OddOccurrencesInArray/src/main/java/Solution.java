2ed8f1f5fe7a9156c3c409fe8fc2c9e9322ae4f1
LeonardoBayona <leo@branham.org>
2017-11-10 00:58:29 -0500

    OddOccurrencesInArray

--- L2-OddOccurrencesInArray/src/main/java/Solution.java
+++ L2-OddOccurrencesInArray/src/main/java/Solution.java
@@ -1,4 +1,5 @@
 import java.util.BitSet;
+import java.util.HashSet;
 
 public class Solution {
 
@@ -6,20 +7,52 @@ public class Solution {
     // https://github.com/LeonardoBayona/codility/tree/master/L2-OddOccurrencesInArray
 
     // Results:
+    // https://codility.com/demo/results/trainingPAXQZU-X3C/
 
     public int solution(int[] A) {
         return calculateOddOccurrencesInArray(A);
     }
 
     public int calculateOddOccurrencesInArray(int[] array) {
-        // weird, but this is O(1), all are false by default
+
+        // Let AP : paired values of A
+        // Let Sum(AP) : (Sum : 0 <= i < N/2 : AP[i])
+        // Let UnPaired(AP) : unique unpaired value of A
+        // Let Sum(A) : (Sum : 0 <= i < N : A[i])
+        // Sum(A) = 2 * Sum(AP) + UnPaired(AP)
+        //
+        // Sum(A) % 2 = Unpaired(AP) % 2
+        //
+        // Xor(A) = Xor(Xor(AP, AP), UnPaired(AP))
+        // Xor(AP, AP) = 0
+        // Xor(A) = UnPaired(AP)
+
+        int xor = 0;
+        for(int i = 0; i < array.length; i++) {
+            xor ^= array[i];
+        }
+        return xor;
+    }
+
+    public int calculateOddOccurrencesInArray4(int[] array) {
+        HashSet<Integer> unpaired = new HashSet<>();
+
+        for(int i = 0; i < array.length; i++) {
+            if(!unpaired.add(array[i])) {
+                unpaired.remove(array[i]);
+            }
+        }
+
+        return unpaired.stream().findFirst().get();
+    }
+
+    public int calculateOddOccurrencesInArray3(int[] array) {
         BitSet unpaired = new BitSet(1000000000);
 
         for(int i = 0; i < array.length; i++) {
             unpaired.set(array[i], !unpaired.get(array[i]));
         }
 
-        // weird, but this is O(1), just the unpaired value is true
         for(int k = 0; k <= 1000000000; k++) {
             if(unpaired.get(k)) {
                 return k;
@@ -30,14 +63,12 @@ public class Solution {
     }
 
     private int calculateOddOccurrencesInArray2(int[] array) {
-        // weird, but this is O(1), all are false by default
         boolean[] unpaired = new boolean[1000000000];
 
         for(int i = 0; i < array.length; i++) {
             unpaired[array[i]] = !unpaired[array[i]];
         }
 
-        // weird, but this is O(1), just the unpaired value is true
         for(int k = 0; k <= 1000000000; k++) {
             if(unpaired[k]) {
                 return k;
