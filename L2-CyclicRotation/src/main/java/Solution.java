431990f2f96b5a84569e23bd03171e052b1d2c2d
LeonardoBayona <leo@branham.org>
2017-11-10 11:52:04 -0500

    CyclicRotation

--- L2-CyclicRotation/src/main/java/Solution.java
+++ L2-CyclicRotation/src/main/java/Solution.java
@@ -12,7 +12,26 @@ public class Solution {
     }
 
     public int[] calculateCyclicRotation(int[] array, int shift) {
+        if(shift == 0 || shift == array.length || array.length <= 1) {
+            // nothing to do
+            return array;
+        }
 
+        shift = shift % array.length;
+
+        int previousElement = array[0];
+        int nextIndex = shift;
+        assert shift < array.length;
+        for(int i = 0; i < array.length; i++) {
+            int currentElement = array[nextIndex];
+            array[nextIndex] = previousElement;
+
+            // prepare to next shift
+            previousElement = currentElement;
+            nextIndex = (nextIndex + shift) % array.length;
+        }
+
+        return array;
     }
 
 }
--- L2-CyclicRotation/src/test/java/SolutionTest.java
+++ L2-CyclicRotation/src/test/java/SolutionTest.java
@@ -10,6 +10,17 @@ public class SolutionTest {
         testSolution(new int[] { 0, 0, 0 }, 1, new int[] { 0, 0, 0 });
         testSolution(new int[] { 1, 2, 3, 4 }, 4, new int[] { 1, 2, 3, 4 });
 
+        testSolution(new int[] { }, 0, new int[] { });
+        testSolution(new int[] { }, 1, new int[] { });
+        testSolution(new int[] { }, 5, new int[] { });
+
+        testSolution(new int[] { 1 }, 0, new int[] { 1 });
+        testSolution(new int[] { 1 }, 1, new int[] { 1 });
+        testSolution(new int[] { 1 }, 5, new int[] { 1 });
+
+        testSolution(new int[] { 1, 2 }, 0, new int[] { 1, 2 });
+        testSolution(new int[] { 1, 2 }, 1, new int[] { 2, 1 });
+        testSolution(new int[] { 1, 2 }, 2, new int[] { 1, 2 });
     }
 
     private void testSolution(int[] array, int shift, int[] expected) {
