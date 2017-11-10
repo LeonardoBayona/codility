bc822846b3f9be977a23f6f81530795f95bf47e5
LeonardoBayona <leo@branham.org>
2017-11-10 14:57:05 -0500

    TapeEquilibrium

---
+++ L3-TapeEquilibrium/src/main/java/Solution.java
@@ -0,0 +1,18 @@
+
+public class Solution {
+
+    // Source code with unit tests:
+    // https://github.com/LeonardoBayona/codility/tree/master/L2-TapeEquilibrium
+
+    // Results:
+    // https://codility.com/demo/results/trainingYWESPR-3XD/
+
+    public int[] solution(int[] A, int K) {
+        return calculateTapeEquilibrium(A, K);
+    }
+
+    public int[] calculateTapeEquilibrium(int[] array, int shift) {
+
+    }
+
+}
