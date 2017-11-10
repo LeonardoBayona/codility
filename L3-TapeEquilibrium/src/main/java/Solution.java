dca4d9492feba3f2fcde7b1617e410c05508c3a1
LeonardoBayona <leo@branham.org>
2017-11-10 15:43:06 -0500

    TapeEquilibrium

--- L3-TapeEquilibrium/src/main/java/Solution.java
+++ L3-TapeEquilibrium/src/main/java/Solution.java
@@ -5,14 +5,42 @@ public class Solution {
     // https://github.com/LeonardoBayona/codility/tree/master/L2-TapeEquilibrium
 
     // Results:
-    // https://codility.com/demo/results/trainingYWESPR-3XD/
+    //
 
-    public int[] solution(int[] A, int K) {
-        return calculateTapeEquilibrium(A, K);
+    public int solution(int[] A) {
+        return calculateTapeEquilibrium(A);
     }
 
-    public int[] calculateTapeEquilibrium(int[] array, int shift) {
+    public int calculateTapeEquilibrium(int[] array) {
+        assert array.length >= 2;
 
+        long[] firstSums = new long[array.length];
+        long[] lastSums = new long[array.length];
+        calculateSums(array, firstSums, lastSums);
+        return findMinDifference(array, firstSums, lastSums);
+    }
+
+    private void calculateSums(int[] array, long[] firstSums, long[] lastSums) {
+        long firstSum = 0;
+        long lastSum = 0;
+        for(int i = 1; i < array.length; i++) {
+            firstSum += array[i - 1];
+            lastSum += array[array.length - i];
+
+            firstSums[i] = firstSum;
+            lastSums[array.length - i] = lastSum;
+        }
+    }
+
+    private int findMinDifference(int[] array, long[] firstSums, long[] lastSums) {
+        long minDiff = Long.MAX_VALUE;
+        for(int k = 1; k < array.length; k++) {
+            long currentDiff = Math.abs(firstSums[k] - lastSums[k]);
+            if(currentDiff < minDiff) {
+                minDiff = currentDiff;
+            }
+        }
+        return (int) minDiff;
     }
 
 }
--- L3-TapeEquilibrium/src/test/java/SolutionTest.java
+++ L3-TapeEquilibrium/src/test/java/SolutionTest.java
@@ -6,17 +6,19 @@ public class SolutionTest {
 
     @Test
     public void test1() {
-        testSolution(new int[] { 3, 8, 9, 7, 6 }, 3, new int[] { 9, 7, 6, 3, 8 });
+        testSolution(new int[] { 3, 1, 2, 4, 3 }, 1);
 
+        testSolution(new int[] { -6, 19, 40, -88, 80, -88, -62, -30, -15, -61, 58 }, 57);
 
+        testSolution(new int[] { 1, 2 }, 1);
     }
 
-    private void testSolution(int[] array, int shift, int[] expected) {
+    private void testSolution(int[] array, int expected) {
         // given
         Solution solution = new Solution();
 
         // when
-        int[] response = solution.calculateTapeEquilibrium(array, shift);
+        int response = solution.calculateTapeEquilibrium(array);
 
         // then
         assertThat(response).isEqualTo(expected);
