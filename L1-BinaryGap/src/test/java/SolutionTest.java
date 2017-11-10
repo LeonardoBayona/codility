e01831277c0fa524ad42d5a30a7c278a11484964
LeonardoBayona <leo@branham.org>
2017-11-09 21:16:14 -0500

    L1-BinaryGap

--- L1-BinaryGap/src/test/java/SolutionTest.java
+++ L1-BinaryGap/src/test/java/SolutionTest.java
@@ -6,35 +6,35 @@ public class SolutionTest {
 
     @Test
     public void test1() {
-        testBinaryGap(0b1001, 2);
-        testBinaryGap(0b1000010001, 4);
-        testBinaryGap(0b10100, 1);
-        testBinaryGap(0b1111, 0);
+        testSolution(0b1001, 2);
+        testSolution(0b1000010001, 4);
+        testSolution(0b10100, 1);
+        testSolution(0b1111, 0);
 
-        testBinaryGap(0b0, 0);
+        testSolution(0b0, 0);
 
-        testBinaryGap(0b10, 0);
-        testBinaryGap(0b1000, 0);
-        testBinaryGap(0b1, 0);
+        testSolution(0b10, 0);
+        testSolution(0b1000, 0);
+        testSolution(0b1, 0);
 
-        testBinaryGap(0b11, 0);
-        testBinaryGap(0b101, 1);
-        testBinaryGap(0b1000001, 5);
+        testSolution(0b11, 0);
+        testSolution(0b101, 1);
+        testSolution(0b1000001, 5);
 
-        testBinaryGap(0b101000, 1);
-        testBinaryGap(0b1000001000, 5);
+        testSolution(0b101000, 1);
+        testSolution(0b1000001000, 5);
 
-        testBinaryGap(0b10100000001, 7);
-        testBinaryGap(0b100000100000001, 7);
+        testSolution(0b10100000001, 7);
+        testSolution(0b100000100000001, 7);
 
-        testBinaryGap(0b100000000010100000001, 9);
-        testBinaryGap(0b1000000000100000100000001, 9);
+        testSolution(0b100000000010100000001, 9);
+        testSolution(0b1000000000100000100000001, 9);
 
-        testBinaryGap(0b1000100000000010100000001, 9);
-        testBinaryGap(0b10001000000000100000100000001, 9);
+        testSolution(0b1000100000000010100000001, 9);
+        testSolution(0b10001000000000100000100000001, 9);
     }
 
-    private void testBinaryGap(int number, int expected) {
+    private void testSolution(int number, int expected) {
         // given
         Solution solution = new Solution();
 
