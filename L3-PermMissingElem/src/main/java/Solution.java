2bdc6c6c7eda094bdfd2a4c14cd100b66d9d402b
LeonardoBayona <leo@branham.org>
2017-11-10 14:38:45 -0500

    PermMissingElem

--- L3-PermMissingElem/src/main/java/Solution.java
+++ L3-PermMissingElem/src/main/java/Solution.java
@@ -12,6 +12,13 @@ public class Solution {
     }
 
     public int calculatePermMissingElem(int[] array) {
-
+        int totalSum = 0;
+        int actualSum = 0;
+        for(int i = 0; i < array.length; i++) {
+            totalSum += (i + 1);
+            actualSum += array[i];
+        }
+        totalSum += (array.length + 1);
+        return totalSum - actualSum;
     }
 }
--- L3-PermMissingElem/src/test/java/SolutionTest.java
+++ L3-PermMissingElem/src/test/java/SolutionTest.java
@@ -6,9 +6,25 @@ public class SolutionTest {
 
     @Test
     public void test1() {
+        testSolution(new int[] { 2, 3, 4, 5 }, 1);
+        testSolution(new int[] { 4, 3, 1, 5 }, 2);
+        testSolution(new int[] { 2, 4, 1, 5 }, 3);
         testSolution(new int[] { 2, 3, 1, 5 }, 4);
+        testSolution(new int[] { 2, 3, 1, 4 }, 5);
 
+        testSolution(new int[] { }, 1);
 
+        testSolution(new int[] { 1 }, 2);
+        testSolution(new int[] { 2 }, 1);
+
+        testSolution(new int[] { 1, 2 }, 3);
+        testSolution(new int[] { 1, 3 }, 2);
+        testSolution(new int[] { 3, 2 }, 1);
+
+        testSolution(new int[] { 1, 2, 3 }, 4);
+        testSolution(new int[] { 1, 2, 4 }, 3);
+        testSolution(new int[] { 1, 3, 4 }, 2);
+        testSolution(new int[] { 3, 2, 4 }, 1);
     }
 
     private void testSolution(int[] array, int expected) {
