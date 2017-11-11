63cc47fbf96c0bd2f7ba413f4724dcb7f63b89a7
LeonardoBayona <leo@branham.org>
2017-11-11 00:08:00 -0500

    FrogRiverOne

--- L4-FrogRiverOne/src/main/java/Solution.java
+++ L4-FrogRiverOne/src/main/java/Solution.java
@@ -11,8 +11,21 @@ public class Solution {
         return calculateFrogRiverOne(X, A);
     }
 
-    public int calculateFrogRiverOne(int position, int[] array) {
+    public int calculateFrogRiverOne(int targetPosition, int[] array) {
+        int positionsReadyCount = 0;
+        int[] positionsReady = new int[targetPosition + 1];
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] <= targetPosition && positionsReady[array[i]] == 0) {
+                positionsReady[array[i]] = array[i];
+                positionsReadyCount++;
 
+                if(positionsReadyCount == targetPosition) {
+                    return i;
+                }
+            }
+        }
+
+        return -1;
     }
 
 }
--- L4-FrogRiverOne/src/test/java/SolutionTest.java
+++ L4-FrogRiverOne/src/test/java/SolutionTest.java
@@ -8,14 +8,33 @@ public class SolutionTest {
     public void test1() {
         testSolution(5, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }, 6);
 
+        testSolution(2, new int[] { 1 }, -1);
+        testSolution(1, new int[] { 1 }, 0);
+
+        testSolution(2, new int[] { 1, 1 }, -1);
+        testSolution(2, new int[] { 2, 1 }, 1);
+        testSolution(2, new int[] { 1, 1, 2 }, 2);
+        testSolution(3, new int[] { 2, 1 }, -1);
+
+        testSolution(3, new int[] { 1, 1, 2 }, -1);
+        testSolution(2, new int[] { 2, 1, 3 }, 1);
+        testSolution(3, new int[] { 2, 1, 3 }, 2);
+        testSolution(2, new int[] { 1, 1, 2 }, 2);
+        testSolution(3, new int[] { 2, 1, 2 }, -1);
+
+        testSolution(6, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }, -1);
+        testSolution(4, new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }, 4);
+        testSolution(3, new int[] { 1, 3, 1, 3, 2, 3, 3, 3 }, 4);
+        testSolution(2, new int[] { 1, 3, 1, 2, 2, 3, 2, 2 }, 3);
+        testSolution(1, new int[] { 1, 1, 1, 1, 1, 1, 1, 1 }, 0);
     }
 
-    private void testSolution(int position, int[] array, int expected) {
+    private void testSolution(int targetPosition, int[] array, int expected) {
         // given
         Solution solution = new Solution();
 
         // when
-        int response = solution.calculateFrogRiverOne(position, array);
+        int response = solution.calculateFrogRiverOne(targetPosition, array);
 
         // then
         assertThat(response).isEqualTo(expected);
