35cb0a3dcd17f71b031e99dde8a46d982020e3c6
LeonardoBayona <leo@branham.org>
2017-11-12 14:53:26 -0500

    MaxCounters

--- L4-MaxCounters/src/test/java/SolutionTest.java
+++ L4-MaxCounters/src/test/java/SolutionTest.java
@@ -8,6 +8,34 @@ public class SolutionTest {
     public void test1() {
         testSolution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 }, new int[] { 3, 2, 2, 4, 2 });
 
+        testSolution(1, new int[] { 1 }, new int[] { 1 });
+        testSolution(1, new int[] { 2 }, new int[] { 0 });
+
+        testSolution(1, new int[] { 1, 1 }, new int[] { 2 });
+        testSolution(1, new int[] { 2, 1 }, new int[] { 1 });
+        testSolution(1, new int[] { 1, 2 }, new int[] { 1 });
+        testSolution(1, new int[] { 2, 2 }, new int[] { 0 });
+
+        testSolution(1, new int[] { 1, 1, 1 }, new int[] { 3 });
+        testSolution(1, new int[] { 2, 1, 1 }, new int[] { 2 });
+        testSolution(1, new int[] { 1, 2, 2 }, new int[] { 1 });
+        testSolution(1, new int[] { 2, 2, 2 }, new int[] { 0 });
+
+
+
+
+        testSolution(2, new int[] { 1 }, new int[] { 1, 0 });
+        testSolution(2, new int[] { 2 }, new int[] { 0, 1 });
+        testSolution(2, new int[] { 3 }, new int[] { 0, 0 });
+
+        testSolution(3, new int[] { 1 }, new int[] { 1, 0, 0 });
+        testSolution(3, new int[] { 2 }, new int[] { 0, 1, 0 });
+        testSolution(3, new int[] { 3 }, new int[] { 0, 0, 1 });
+        testSolution(3, new int[] { 4 }, new int[] { 0, 0, 0 });
+
+
+        testSolution(5, new int[] { 3, 4, 4, 6, 1, 4, 1 }, new int[] { 4, 2, 2, 3, 2 });
+        testSolution(5, new int[] { 3, 4, 4, 6, 1, 4, 6 }, new int[] { 3, 3, 3, 3, 3 });
 
     }
 
